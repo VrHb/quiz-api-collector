3fef463ae7258a90722ba1baa1e506946640efdb
Viktor Abdukarimov <viredmi@gmail.com>
2023-10-12 15:40:33 +0300

    Fix db url

--- database.py
+++ database.py
@@ -9,13 +9,12 @@ from sqlalchemy.orm import sessionmaker
 load_dotenv()
 
 PSQL_PASSWORD = os.getenv("PSQL_PASSWORD")
-PSQL_PORT = os.getenv("PSQL_PORT", default="5432")
+PSQL_DB_NAME = os.getenv("DB_NAME")
 
-DATABASE_URL = f"postgresql://postgres:{PSQL_PASSWORD}@localhost:{PSQL_PORT}"
+DATABASE_URL = f"postgresql://postgres:{PSQL_PASSWORD}@psdb/{PSQL_DB_NAME}"
 
 engine = create_engine(
     DATABASE_URL 
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
